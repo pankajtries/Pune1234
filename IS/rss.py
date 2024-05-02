a27cb5c09568a13a71577bb206ca1868aadc9562
pankajtries <141701152+pankajtries@users.noreply.github.com>
2024-05-02 18:32:57 +0530

    Update rss.py

--- IS/rss.py
+++ IS/rss.py
@@ -1,11 +1,9 @@
 import math
 
 def is_prime(n):
-    if n <= 1:
-        return False
-    elif n <= 3:
-        return True
-    elif n % 2 == 0 or n % 3 == 0:
+    if n <= 3:
+        return n > 1
+    if n % 2 == 0 or n % 3 == 0:
         return False
     i = 5
     while i * i <= n:
